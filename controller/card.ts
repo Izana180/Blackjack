5fa4a08f15177c9b9f1354a59c2c505003818740
Izana180 <valleymouth0402@yahoo.co.jp>
2025-05-01 23:09:50 +0900

    スート、数字の一覧を定数化

--- controller/card.ts
+++ controller/card.ts
@@ -1,6 +1,9 @@
 export type Suit = "S" | "H" | "D" | "C";
 export type Rank = "A" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K";
 
+export const ALL_SUITS: Suit[] = ["S", "H", "D", "C"];
+export const ALL_RANKS: Rank[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 export class Card{
     private readonly suit : Suit;
     private readonly rank : Rank;
