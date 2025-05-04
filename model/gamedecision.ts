8c6236b7b02fb50067666504fb9bb75c8ff1bd7b
Izana180 <valleymouth0402@yahoo.co.jp>
2025-05-05 01:11:24 +0900

    アクションを提供するメソッドを追加:

---
+++ model/gamedecision.ts
@@ -0,0 +1,12 @@
+export type playerAction = "bet" | "surrender" | "stand" | "hit" | "double";
+
+// プレイヤーのアクションとサイジングを提供するオブジェクト
+export class GameDecision{
+    private readonly action: playerAction;
+    private readonly amount: Number;
+
+    constructor(action: playerAction, amount: Number){
+        this.action = action;
+        this.amount = amount;
+    }
+}
