ac5ca14a2d50d4856a99a296a419da08cf3c903d
Izana180 <valleymouth0402@yahoo.co.jp>
2025-05-05 14:09:48 +0900

    ランナーを追加

---
+++ controller/bjRunnerBottest.ts
@@ -0,0 +1,27 @@
+import { Table } from "@model/table";
+import { Player } from "@model/player";
+
+// playerリスト
+const players: Player[] = [
+    new Player("Bob", "ai", 400),
+    new Player("Alice", "ai", 400),
+    new Player("Dealer", "house", 0),
+];
+
+// betサイズの選択肢
+const betOptions = [10, 50, 100];
+
+const table = new Table(betOptions, players);
+
+while(table.getGamePhase() !== "gameOver"){
+    table.haveTurn();
+
+    // アクション終了
+    if(table.allPlayerActionsResolved()){
+        const result = table.bjEvaluateAndGetRoundResults();
+        console.log("--------- Result ---------");
+        console.log(result);
+        break;
+    }
+}
+
